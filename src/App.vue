2febfc52110afbe31c86035b2c69a7be9e5bf29c
iponkan <iponkan@gmail.com>
2019-08-22 11:55:18 +0800

    try fix scroll

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,60 @@
 <template>
-  <!-- Make a div wrapped slider,set height and width -->
-  <div style="width:100%;height:100%">
-    <!-- Using the slider component -->
-    <slider ref="slider" :options="options" @slide='slide' @tap='onTap' @init='onInit'>
-      <!-- slideritem wrapped package with the components you need -->
-      <slideritem v-for="(item,index) in someList" :key="index" :style="item.style">{{item.html}}</slideritem>
-      <!-- Customizable loading -->
-      <div slot="loading">loading...</div>
-    </slider>
-  </div>
+  <scroll style="background-color: #f2f2f2;">
+    <!-- Make a div wrapped slider,set height and width -->
+    <div style="width:100%;height:100%">
+      <!-- Using the slider component -->
+      <slider ref="slider" :options="options" @slide='slide' @tap='onTap' @init='onInit'>
+        <!-- slideritem wrapped package with the components you need -->
+        <slideritem v-for="(item,index) in someList" :key="index" :style="item.style">{{item.html}}</slideritem>
+        <!-- Customizable loading -->
+        <div slot="loading">loading...</div>
+      </slider>
+    </div>
+  </scroll>
+
 </template>
 <script>
-    // import slider components
-    import {slider, slideritem} from 'vue-concise-slider'
+  // import slider components
+  import {slider, slideritem} from 'vue-concise-slider'
 
-    export default {
-        el: '#app',
-        data() {
-            return {
-                //data list [array]
-                someList: [
-                    {
-                        html: 'slide1',
-                        style: {
-                            'background': '#1bbc9b'
-                        }
-                    },
-                    {
-                        html: 'slide2',
-                        style: {
-                            'background': '#4bbfc3'
-                        }
-                    },
-                    {
-                        html: 'slide3',
-                        style: {
-                            'background': '#7baabe'
-                        }
-                    }
-                ],
-                //Slider configuration [obj]
-                options: {
-                    currentPage: 0,
-                    direction: 'vertical'
-                }
+  import {scroll} from './components/Scroll'
+
+  export default {
+    el: '#app',
+    data() {
+      return {
+        //data list [array]
+        someList: [
+          {
+            html: 'slide1',
+            style: {
+              'background': '#1bbc9b'
             }
-        },
-        components: {
-            slider,
-            slideritem
+          },
+          {
+            html: 'slide2',
+            style: {
+              'background': '#4bbfc3'
+            }
+          },
+          {
+            html: 'slide3',
+            style: {
+              'background': '#7baabe'
+            }
+          }
+        ],
+        //Slider configuration [obj]
+        options: {
+          currentPage: 0,
+          direction: 'vertical'
         }
+      }
+    },
+    components: {
+      scroll,
+      slider,
+      slideritem
     }
+  }
 </script>
